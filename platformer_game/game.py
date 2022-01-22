43101bffe6c2a48855e6753cd0c06486b8687fba
dvnDC <damcichosz@gmail.com>
2022-01-22 05:21:17 +0100

    Create Fonts class

--- platformer_game/game.py
+++ platformer_game/game.py
@@ -11,6 +11,7 @@ from platforms import Platforms
 from sprite import Sprite
 from menu import Menu
 from file_loader import FileLoader
+from font import Font
 
 
 from pygame.math import Vector2
@@ -21,23 +22,20 @@ pygame.display.set_caption("dvn's game")
 class Game(object):
 
     def __init__(self):
-
         # Settings
-
-        pygame.mixer.init()
-
-        pygame.mixer.music.load('latenight.ogg')
-        pygame.mixer.music.play(0)
-
         self.WIDTH = 640
         self.HEIGHT = 360
+
+        # Sound settings
+        pygame.mixer.init()
+        pygame.mixer.music.load('latenight.ogg')
+        pygame.mixer.music.play(0)
 
         # Config
         self.tps_max = 100
 
         # Initialization
         pygame.init()
-        font = pygame.font.SysFont("Arial", 18)
         self.resolution = (self.screen_width, self.screen_height) = (self.WIDTH, self.HEIGHT)
         self.screen = pygame.display.set_mode(self.resolution, pygame.RESIZABLE)
 
@@ -46,7 +44,7 @@ class Game(object):
         self.scroll = Vector2(0,0)
 
         self.map = Map(self)
-        self.player = Player(self)  # przy inicjalizacji przekazuje playerowi wszystko Player(self)
+        self.player = Player(self)
         self.enemy = Enemy(self)
         self.weapon = Weapon(self)
         self.fire = Fire(self)
@@ -56,34 +54,12 @@ class Game(object):
         self.sprite = Sprite(self)
         self.menu = Menu(self)
         self.file_loader = FileLoader(self)
+        self.font = Font(self)
 
         self.sprite.load_images()
         self.sprite.load_icon()
 
-        def create_fonts(font_sizes_list):
-            "Creates different fonts with one list"
-            fonts = []
-            for size in font_sizes_list:
-                fonts.append(
-                    pygame.font.SysFont("Arial", size))
-            return fonts
-
-        def render(fnt, what, color, where):
-            "Renders the fonts as passed from display_fps"
-            text_to_show = fnt.render(what, 0, pygame.Color(color))
-            self.screen.blit(text_to_show, where)
-
-        def display_fps():
-            "Data that will be rendered and blitted in _display"
-            render(
-                fonts[0],
-                what=str(int(self.tps_clock.get_fps())),
-                color="white",
-                where=(0, 0))
-
-        fonts = create_fonts([32, 16, 14, 8])
         while True:
-
             # Events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -92,7 +68,7 @@ class Game(object):
                     sys.exit(0)
 
             # Ticking
-            self.tps_delta += self.tps_clock.tick() / 1000.0  # zamieniam MS na sekundy
+            self.tps_delta += self.tps_clock.tick() / 1000.0  # convert milliseconds to seconds; 1000 ms = 1 s
             while self.tps_delta > 1 / self.tps_max:
                 self.tick()
                 self.tps_delta -= 1 / self.tps_max
@@ -100,8 +76,7 @@ class Game(object):
             # Rendering/Drawing
             self.screen.fill((0, 0, 0))
             self.draw()
-            display_fps()
-            pygame.display.flip()
+            pygame.display.update()
 
     def tick(self):
         self.player.tick()
@@ -117,6 +92,7 @@ class Game(object):
         self.enemy.draw()
         self.weapon.draw()
         self.fire.draw()
+        self.font.draw()
 
 
 if __name__ == "__main__":
