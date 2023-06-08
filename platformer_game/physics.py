c8a4a62363dcd250b3c9a47635950be1b4dc2d41
Damian Cichosz <damcichosz@gmail.com>
2023-06-08 03:12:44 +0200

    Refactor physics file

--- platformer_game/physics.py
+++ platformer_game/physics.py
@@ -6,54 +6,38 @@ class Physics(object):
         self.game = game
         self.gravity = 0.9
 
-        self.pos = Vector2(0, 0)
+        self.pos = Vector2(300, 170)
         self.vel = Vector2(0, 0)
         self.acc = Vector2(0, 0)
 
-        self.pos.x = 300
-        self.pos.y = 170
         self.width = 20
         self.height = 60
-        self.position = (0, 0, 0, 0)  # x0 x1 y0 y1
-        self.true_scroll = [0,0]
-        self.true_scroll2 = [0,0]
+        self.true_scroll = [0, 0]
+        self.true_scroll2 = [0, 0]
         self.isStanding = False
 
     def standing(self):
         self.game.collision.enemy_collision()
-        if self.game.collision.isBottomCollision == True:
-            self.isStanding = True
-        else:
-            self.isStanding = False
+        self.isStanding = self.game.collision.isBottomCollision
+
+    def set_scroll(self, scroll):
+        game_objects = [self.game.map, self.game.player, self.game.platforms,
+                        self.game.enemy, self.game.fire, self.game.collision,
+                        self.game.weapon]
+
+        for obj in game_objects:
+            obj.scroll.x = scroll[0]
+            obj.scroll.y = scroll[1]
 
     def scroll(self):
-        self.true_scroll[0] += (self.game.player.pos.x - self.true_scroll[0] - 640)/40
-        ###
-        # self.true_scroll2[0] += (self.game.player.pos.x - self.true_scroll[0]-640)
+        self.true_scroll[0] += (self.game.player.pos.x - self.true_scroll[0] - 640) / 40
         self.true_scroll[1] += (self.game.player.pos.y - self.true_scroll[1] - 170)
-        scroll = self.true_scroll.copy()
-        scroll[0] = int(scroll[0])
-        scroll[1] = int(scroll[1])
+        scroll = [int(x) for x in self.true_scroll]
 
-        self.game.map.scroll.x = scroll[0]
-        self.game.map.scroll.y = scroll[1]
-        self.game.player.scroll.x = scroll[0]
-        self.game.player.scroll.y = scroll[1]
-        self.game.scroll.x = scroll[0]
-        self.game.scroll.y = scroll[1]
-        self.game.platforms.scroll.x = scroll[0]
-        self.game.platforms.scroll.y = scroll[1]
-        self.game.enemy.scroll.x = scroll[0]
-        self.game.enemy.scroll.y = scroll[1]
-        self.game.fire.scroll.x = scroll[0]
-        self.game.fire.scroll.y = scroll[1]
-        self.game.collision.scroll.x = scroll[0]
-        self.game.collision.scroll.y = scroll[1]
-        self.game.weapon.scroll.x = scroll[0]
-        self.game.weapon.scroll.y = scroll[1]
+        self.set_scroll(scroll)
 
-        self.game.platforms.true_scroll.x = self.true_scroll2[0] #DO WYRZUCENIA
+        self.game.platforms.true_scroll.x = self.true_scroll2[0]  # To be removed
 
     def tick(self):
         self.scroll()
-
+        # self.standing()
