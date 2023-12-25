bac485ca7378ec419f5c5bb4d0b62b037d3e6bb5
Damian Cichosz <damcichosz@gmail.com>
2023-12-25 02:41:27 +0100

    Update engine class

--- platformer_game/engine.py
+++ platformer_game/engine.py
@@ -8,7 +8,9 @@ class Engine(object):
         self.fonts = self.create_fonts([32, 16, 14, 8])
 
     def create_fonts(self, font_sizes_list):
-        "Creates different fonts with one list"
+        """
+        Creates different fonts with one list
+        """
         self.fonts = []
         for size in font_sizes_list:
             self.fonts.append(
@@ -16,12 +18,16 @@ class Engine(object):
         return self.fonts
 
     def render(self, fnt, what, color, where):
-        "Renders the fonts as passed from display_fps"
+        """
+        Renders the fonts as passed from display_fps
+        """
         text_to_show = fnt.render(what, 0, pygame.Color(color))
         self.game.screen.blit(text_to_show, where)
 
     def display_fps(self):
-        "Data that will be rendered and blitted in _display"
+        """
+        Data that will be rendered and blitted in _display
+        """
         self.render(
             self.fonts[0],
             what=str(int(self.game.tps_clock.get_fps())),
